c7c84b9a0ede73c0db6b64072e12c0fcf0ab1bc8
adiinan <muh.adinan9@gmail.com>
2024-11-04 10:51:35 +0530

    Add files via upload

---
+++ oddoreven(listlength).dart
@@ -0,0 +1,8 @@
+void main() {
+  List list = [12, 34, 67, 78, 89, 34, 56];
+  if (list.length % 2 == 0) {
+    print("even");
+  } else {
+    print("odd");
+  }
+}
---
+++ replaceastring.dart
@@ -0,0 +1,5 @@
+void main() {
+  String date = "17,3,20";
+  var kvr = date.replaceAll(",", "/");
+  print(kvr);
+}
